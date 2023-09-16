78ac62b04b7fb776e2b07b5bf4ea5597d4c63a0b
f <dimagolub554@gmail.com>
2023-09-16 18:10:32 +0300

    adding_steam_rule_window

--- dwm/config.h
+++ dwm/config.h
@@ -30,7 +30,7 @@ static const Rule rules[] = {
 	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "TelegramDesktop",  NULL,       NULL,       1 << 6,       0,           -1 },
         { "discord",  NULL,       NULL,       1 << 6,       0,           -1 },
-	{ "Steam",  NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "steam",  NULL,       NULL,       1 << 5,       0,           -1 },
 
 
 };
