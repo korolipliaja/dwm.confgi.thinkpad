70f13a479bd9f52501b280ce2cb65414659d97a9
f <dimagolub554@gmail.com>
2023-09-22 22:02:17 +0300

    fishcoolour

--- utility/fish/functions/proton.fish
+++ utility/fish/functions/proton.fish
@@ -1,4 +1,4 @@
-function proton --wraps='env /home/kpliaja/PortWINE/PortProton/data/scripts/start.sh' --description 'alias proton=env /home/kpliaja/PortWINE/PortProton/data/scripts/start.sh'
-  env /home/kpliaja/PortWINE/PortProton/data/scripts/start.sh $argv
+﻿function proton --wraps='env /home/f/PortWINE/PortProton/data/scripts/start.sh' --description 'alias proton=env /home/f/PortWINE/PortProton/data/scripts/start.sh'
+  env /home/f/PortWINE/PortProton/data/scripts/start.sh $argv
         
 end
