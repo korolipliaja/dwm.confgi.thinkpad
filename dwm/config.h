6e4fdd2e96709166b1082d2dff1d7f4095da290c
f <dimagolub554@gmail.com>
2023-11-03 23:49:33 +0200

    dmenu flexipatch

--- dwm/config.h
+++ dwm/config.h
@@ -31,6 +31,7 @@ static const Rule rules[] = {
 	{ "TelegramDesktop",  NULL,       NULL,       1 << 6,       0,           -1 },
         { "discord",  NULL,       NULL,       1 << 6,       0,           -1 },
 	{ "steam",  NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Ledger Live",  NULL,       NULL,       1 << 4,       0,           -1 },
 
 
 };
